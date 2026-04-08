deaf64a9c08f4155d6094868cf25379fe2b04642
KarynaMisnik <karinamisnik94@gmail.com>
2026-04-08 14:30:00 +0300

    prepare env for db implementation

--- src/Task.java
+++ src/Task.java
@@ -1,12 +1,21 @@
 package src;
 
 public class Task {
+    private int id;
     private String title;
     private boolean done;
 
     public Task(String title) {
         this.title = title;
         this.done = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
